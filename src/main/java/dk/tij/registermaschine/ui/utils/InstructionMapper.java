5b0dfa643308fbed5b0b6f02724562e99132a821
TιJ <78140568+TiJ-code@users.noreply.github.com>
2026-04-02 18:03:37 +0200

    (#5) Added Better Instruction Usage
    Displaying info about operands that are not implicit in the instruction
    info.

--- src/main/java/dk/tij/registermaschine/ui/utils/InstructionMapper.java
+++ src/main/java/dk/tij/registermaschine/ui/utils/InstructionMapper.java
@@ -1,5 +1,6 @@
 package dk.tij.registermaschine.ui.utils;
 
+import dk.tij.registermaschine.api.compilation.compiling.OperandType;
 import dk.tij.registermaschine.api.config.ConfigInstruction;
 import dk.tij.registermaschine.api.instructions.IInstructionSet;
 
@@ -8,7 +9,23 @@ import java.util.List;
 public final class InstructionMapper {
     public static List<InstructionDefinition> toDocList(IInstructionSet set) {
         return set.getInstructions().stream()
-                .map(i -> new InstructionDefinition(i.mnemonic(), i.description()))
+                .map(i -> {
+                    StringBuilder nameBuilder = new StringBuilder(i.mnemonic().toUpperCase());
+                    nameBuilder.append(' ');
+                    for (var o : i.operands()) {
+                        if (o.isImplicit()) continue;
+
+                        switch (o.type()) {
+                            case OperandType.IMMEDIATE -> nameBuilder.append("#?");
+                            case OperandType.REGISTER ->  nameBuilder.append("R?");
+                            case OperandType.LABEL ->     nameBuilder.append("label");
+                        }
+
+                        nameBuilder.append(' ');
+                    }
+
+                    return new InstructionDefinition(nameBuilder.toString(), i.description());
+                })
                 .toList();
     }
 
@@ -18,5 +35,5 @@ public final class InstructionMapper {
                 .toList();
     }
 
-    public record InstructionDefinition(String name, String description) {}
+    public record InstructionDefinition(String usage, String description) {}
 }
